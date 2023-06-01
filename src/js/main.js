3c1ebe1c7e196ebee9fbc4129c480fa6cbdd7915
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-06-01 15:59:27 +0300

    after all

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-// alert("This time without js. Will do it for the next step project");
+let mobileMenuIcon = document.querySelector(".header__nav-wrapper");
+
+let burgerMenu = document.querySelector(".header__nav__icon");
+let crossIcon = document.querySelector(".header__nav__cross-icon");
+let mobileMenu = document.querySelector(".header__nav");
+let mobileMenuItems = document.querySelectorAll(".header__nav__item");
+
+let status = false;
+mobileMenuIcon.addEventListener("click", (event) => {
+  if (status) {
+    burgerMenu.classList.remove("header__nav__icon--passive");
+    crossIcon.classList.remove("header__nav__cross-icon--active");
+    mobileMenu.classList.remove("header__nav--active");
+    status = false;
+  } else {
+    status = true;
+    burgerMenu.classList.add("header__nav__icon--passive");
+    crossIcon.classList.add("header__nav__cross-icon--active");
+    mobileMenu.classList.add("header__nav--active");
+  }
+});
