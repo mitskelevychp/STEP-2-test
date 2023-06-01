30cd6636a1d6db2dc8905e1f470309e7c07b25d7
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-06-01 16:40:00 +0300

    corrected mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,95 @@
-let mobileMenuIcon = document.querySelector(".header__nav-wrapper");
+let mobileMenuIcon = document.querySelector(
+  ".header__nav-wrapper"
+); /* toggleButton */
 
 let burgerMenu = document.querySelector(".header__nav__icon");
 let crossIcon = document.querySelector(".header__nav__cross-icon");
-let mobileMenu = document.querySelector(".header__nav");
-let mobileMenuItems = document.querySelectorAll(".header__nav__item");
-
+let mobileMenu = document.querySelector(".header__nav"); /* menu */
 let status = false;
-mobileMenuIcon.addEventListener("click", (event) => {
-  if (status) {
-    burgerMenu.classList.remove("header__nav__icon--passive");
-    crossIcon.classList.remove("header__nav__cross-icon--active");
-    mobileMenu.classList.remove("header__nav--active");
-    status = false;
-  } else {
+
+document.addEventListener("DOMContentLoaded", function () {
+  mobileMenuIcon.addEventListener("click", (event) => {
+    if (status) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    let targetElement = event.target;
+    if (
+      !mobileMenu.contains(targetElement) &&
+      !mobileMenuIcon.contains(targetElement)
+    ) {
+      closeMenu();
+    }
+  });
+
+  mobileMenu.addEventListener("click", function (event) {
+    let targetElement = event.target;
+    if (
+      !targetElement.classList.contains("header__nav__item") ||
+      !targetElement.classList.contains("header__nav")
+    ) {
+      closeMenu();
+    }
+  });
+
+  function openMenu() {
     status = true;
     burgerMenu.classList.add("header__nav__icon--passive");
     crossIcon.classList.add("header__nav__cross-icon--active");
     mobileMenu.classList.add("header__nav--active");
   }
+
+  function closeMenu() {
+    status = false;
+    burgerMenu.classList.remove("header__nav__icon--passive");
+    crossIcon.classList.remove("header__nav__cross-icon--active");
+    mobileMenu.classList.remove("header__nav--active");
+  }
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   mobileMenuIcon.addEventListener("click", (event) => {
+//     if (status) {
+//       burgerMenu.classList.remove("header__nav__icon--passive");
+//       crossIcon.classList.remove("header__nav__cross-icon--active");
+//       mobileMenu.classList.remove("header__nav--active");
+//       status = false;
+//     } else {
+//       status = true;
+//       burgerMenu.classList.add("header__nav__icon--passive");
+//       crossIcon.classList.add("header__nav__cross-icon--active");
+//       mobileMenu.classList.add("header__nav--active");
+//     }
+//   });
+
+//   document.addEventListener("click", function (event) {
+//     let targetElement = event.target;
+//     if (
+//       !mobileMenu.contains(targetElement) &&
+//       !mobileMenuIcon.contains(targetElement)
+//     ) {
+//       burgerMenu.classList.remove("header__nav__icon--passive");
+//       crossIcon.classList.remove("header__nav__cross-icon--active");
+//       mobileMenu.classList.remove("header__nav--active");
+//       status = false;
+//     }
+//   });
+// });
+
+// mobileMenuIcon.addEventListener("click", (event) => {
+//   if (status) {
+//     burgerMenu.classList.remove("header__nav__icon--passive");
+//     crossIcon.classList.remove("header__nav__cross-icon--active");
+//     mobileMenu.classList.remove("header__nav--active");
+//     status = false;
+//   } else {
+//     status = true;
+//     burgerMenu.classList.add("header__nav__icon--passive");
+//     crossIcon.classList.add("header__nav__cross-icon--active");
+//     mobileMenu.classList.add("header__nav--active");
+//   }
+// });
